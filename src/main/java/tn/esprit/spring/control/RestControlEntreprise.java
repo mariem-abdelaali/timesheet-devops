35a90334d4de3a5db4b6132d9dd22934201d7164
mabdelaali <mabdelaali@vermeg.com>
2022-06-22 13:47:01 +0100

    test build

--- src/main/java/tn/esprit/spring/control/RestControlEntreprise.java
+++ src/main/java/tn/esprit/spring/control/RestControlEntreprise.java
@@ -41,7 +41,7 @@ public class RestControlEntreprise {
     
     
   
-    // http://localhost:8082/getEntrepriseById/1
+    // http://localhost:8082/getEntrepriseById/{identreprise}
     @GetMapping("/getEntrepriseById/{identreprise}")
     @ResponseBody
 	public Entreprise getEntrepriseById(@PathVariable("identreprise") int entrepriseId) {
