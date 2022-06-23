9486de62dbcae4eeefc8792ece4009506bd64504
mabdelaali <mabdelaali@vermeg.com>
2022-06-23 01:15:17 +0100

    Add relation dep to Entreprise

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -1,6 +1,7 @@
 package tn.esprit.spring.services;
 import java.util.List;
 
+import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
 
 public interface IEntrepriseService {
@@ -11,4 +12,8 @@ public interface IEntrepriseService {
 	Entreprise updateEntreprise(Entreprise entreprise);
 	List<Entreprise> retrieveAllEntreprise(); 
 	Entreprise retrieveEntrepriseById(int id);
+	public int ajouterDepartement(Departement dep);
+	public void affecterDepartementAEntreprise(int depId, int entrepriseId);
+	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);	
+	//public boolean deleteDepartementById(int depId);
 }
